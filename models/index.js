dba6207b03414958ab8b66b5ebffa77adaa9d86a
unknown <git@internode.on.net>
2022-09-03 16:51:43 +0930

    commit of working files still in progress

--- models/index.js
+++ models/index.js
@@ -4,6 +4,27 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
+
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+})
+
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+})
+
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  as: 'productTag_tag',
+  foreignKey: 'product_id'
+})
+
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  as: 'productTag_product',
+  foreignKey: 'tag_id'
+})
+
 // Products belongsTo Category
 
 // Categories have many Products
